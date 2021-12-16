4079bd20d74bd97bdf33716c3fec1614525c59a5
Ross Murray <rossmurray@protonmail.com>
2021-12-16 14:35:50 +1100

    feat: rewrite of day 3A with my latest learnings from the Rust in Motion coursework

--- src/days/day_three_a.rs
+++ src/days/day_three_a.rs
@@ -1,158 +1,121 @@
-pub fn get_puzzle_input() -> core::str::Lines<'static> {
-    include_str!("../inputs/day_three_a.txt")
-        .lines()
+const WIDTH: usize = 12;
+
+/// Determining the gamma and epsilon values is very simmilar logic.
+/// Defining an Enum with variants of each allows us to write a function
+/// that will accept either, and perform only the relevant actions for
+/// the provided variant.
+#[derive(Debug)]
+enum DiagnosticRate {
+    Gamma(usize),
+    Epsilon(usize),
 }
 
+/// A struct to represent our Diagnostic.
+/// This uses the DiagnosticRate enum variants for types.
+#[derive(Debug)]
 struct Diagnostic {
-    // a zero-indexed width of the data lines
-    width: usize,
-    // our calculated gamma rate, if performed yet
-    gamma: Option<usize>,
-    // our calculated epsilon rate, if performed yet
-    epsilon: Option<usize>,
-    // our calculated oxygen generator rating, if avail
-    oxygen: Option<usize>,
-    // our calculated co2 scrubber rating, if avail
-    scrubber: Option<usize>,
-    // our calculated life support rating, if avail
-    life_support: Option<usize>,
-    // our final answer for a, if prepared yet
-    answer_a: Option<usize>,
-    // our final answer for b, if prepared yet
-    answer_b: Option<usize>,
-    //
-    zeros: Option<Vec<usize>>,
-    ones: Option<Vec<usize>>,
+    data: String,
+    gamma_rate: DiagnosticRate,
+    epsilon_rate: DiagnosticRate,
+    power_consumption: usize,
 }
 
+/// Implements the associated functions & methods of the Diagnostic struct
 impl Diagnostic {
 
-    fn new() -> Diagnostic {
-        Diagnostic {
-            width: 12,
-            gamma: None,
-            epsilon: None,
-            oxygen: None,
-            scrubber: None,
-            life_support: None,
-            answer_a: None,
-            answer_b: None,
-            zeros: None,
-            ones: None,
-        }
+    /// allocate the input binary data for the diagnostic
+    fn get_data(&mut self) -> &mut Self {
+        self.data = std::fs::read_to_string("src/inputs/day_three_a.txt")
+            .expect("Unable to retrieve input data.");
+        self
     }
 
-    fn get_gamma_and_epsilon(self: &mut Self) -> &mut Self {
+    /// Generate a Diagnostic's rate based on which enum variant is provided
+    fn generate_rate(&mut self, rate: DiagnosticRate) -> &mut Self {
+        let lines = self.data.lines();
 
-        let puzzle_input = get_puzzle_input();
+        // instantiate result vectors for later comparison
+        let mut zeroes: Vec<usize> = vec![0; WIDTH];
+        let mut ones: Vec<usize> = vec![0; WIDTH];
 
-        let mut zeros = vec![0; self.width];
-        let mut ones = vec![0; self.width];
-
-        for line in puzzle_input {
-
-            let line_bytes = line.as_bytes().iter().enumerate();
-
-            for (i, byte) in line_bytes {
-                
-                // b'0' is matching on a byte-literal.
-                match byte {
-                    b'0' => {
-                        zeros[i] = zeros[i] + 1;
-                    },
-                    b'1' => {
-                        ones[i] = ones[i] + 1;
-                    },
-                    b'\n' => { 
-                        // do nothing on empty lines
-                    },
-                    _ => {
-                        // panic if anything else is found.
-                        panic!("invalid number found");
-                    },
+        // loop over our lines and increment our result vectors as we go
+        for line in lines {
+            let mut char_indicies = line.char_indices();
+            for (idx, char_value) in char_indicies {
+                match char_value {
+                    '0' => zeroes[idx] += 1,
+                    '1' => ones[idx] += 1,
+                    // there should never be anything found other than a zero or one.
+                    _ => unreachable!(),
                 }
             }
         }
 
-        // calculate the gamma
-        let mut gamma_rate_binary = String::new();
-        let mut epsilon_rate_binary = String::new();
-        for (i, &count) in zeros.iter().enumerate() {
-            if count.gt(&ones[i]) {
-                gamma_rate_binary.push('0');
-                epsilon_rate_binary.push('1');
-            } else {
-                gamma_rate_binary.push('1');
-                epsilon_rate_binary.push('0');
+        // compare our two result vectors to create our gamma value
+        let mut gamma: String = String::new();
+        for idx in 0..WIDTH {
+            match rate {
+                DiagnosticRate::Gamma(_) => {
+                    if zeroes[idx] > ones[idx] { gamma.push_str("0")} else { gamma.push_str("1") };
+                },
+                DiagnosticRate::Epsilon(_) => {
+                    if zeroes[idx] > ones[idx] { gamma.push_str("1")} else { gamma.push_str("0") };
+                },
             }
         }
 
-        // add the zeros and ones to our struct for later use
-        self.zeros = Some(zeros);
-        self.ones = Some(ones);
+        match rate {
+            DiagnosticRate::Gamma(_) => {
+                // set our gamma value by parsing our string slice into radix 2
+                self.gamma_rate = DiagnosticRate::Gamma(
+                    usize::from_str_radix(&gamma, 2)
+                        .expect("Unable to parse gamma value into radix 2.")
+                )
+            },
+            DiagnosticRate::Epsilon(_) => {
+                // set our gamma value by parsing our string slice into radix 2
+                self.epsilon_rate = DiagnosticRate::Epsilon(
+                    usize::from_str_radix(&gamma, 2)
+                        .expect("Unable to parse epsilon value into radix 2.")
+                )
+            },
+        }
 
-        // calculate and set gamma value
-        self.gamma = Some(usize::from_str_radix(&gamma_rate_binary, 2)
-            .expect("Error calculating the gamma_rate_decimal"));
-
-        // calculate and set epsilon value
-        self.epsilon = Some(usize::from_str_radix(&epsilon_rate_binary, 2)
-            .expect("Error calculating the epsilon_rate_decimal"));
-
-        // calculate and set answer_a value
-        // these are safe to unwrap as we added the Some() ourselves
-        self.answer_a = Some(self.gamma.unwrap() * self.epsilon.unwrap());
-
-        // return our updated struct
         self
     }
 
-    fn get_oxygen_rating(self: &mut Self) -> &mut Self {
-
-        // create a vector of the highest bit in each column
-        // 1's win any ties
-        let o = self.ones.as_ref().expect("No ones are found.");
-        let z = self.zeros.as_ref().expect("No zeroes are found.");
-        let mut highest_values: Vec<usize> = Vec::new();
-        for (i, count_of_ones) in o.iter().enumerate() {
-            if count_of_ones.ge(&z[i]) {
-                highest_values.push(1);
-            } else {
-                highest_values.push(0);
-            }
-        };
-
-        let mut input: Vec<&str> = get_puzzle_input().into();
-        let mut current_index = highest_values.iter();
-        
-        while input.len().ne(&1) {
-            let c = current_index.next().expect("Index could not be set.");
-            input.filter(|&x| (x[(c -1)..*c].parse::<usize>().unwrap()).eq(highest_values.iter().nth(*c).expect("No index exists."))).collect();
+    fn get_power_consumption(&mut self) -> &mut Self {
+        if let DiagnosticRate::Gamma(gamma) = self.gamma_rate {
+            if let DiagnosticRate::Epsilon(epsilon) = self.epsilon_rate {
+                self.power_consumption = gamma * epsilon;
+            };
         }
-
-        println!("filtered input: {:#?}", &input);
-
-        todo!()
+        self
     }
 
-    fn get_scrubber_rating(self: &mut Self) -> &mut Self {todo!() }
 
-    fn get_life_support_rating(self: &mut Self) -> &mut Self {todo!()}
+    fn new() -> Diagnostic {
+        let mut diagnostic = Diagnostic {
+            data: Default::default(),
+            gamma_rate: DiagnosticRate::Gamma(Default::default()),
+            epsilon_rate: DiagnosticRate::Epsilon(Default::default()),
+            power_consumption: Default::default(),
+        };
+
+        diagnostic.get_data()
+            .generate_rate(DiagnosticRate::Gamma(Default::default()))
+            .generate_rate(DiagnosticRate::Epsilon(Default::default()))
+            .get_power_consumption();
+
+        diagnostic
+    }
 
 }
 
+/// Our only exported/public function, this will run the Day 3 puzzle solutions
 pub fn run() {
-    
-    let mut diagnostic = Diagnostic::new();
-    diagnostic.get_gamma_and_epsilon();
-    println!(
-        "Day 3A: Final Answer - {:#?}", 
-        diagnostic.answer_a.expect("Unable to calculate answer - 3A."));
+    // instantiate our Diagnostic
+    let diagnostic = Diagnostic::new();
 
-    diagnostic.get_oxygen_rating();
-    diagnostic.get_scrubber_rating();
-    diagnostic.get_life_support_rating();
-    println!(
-        "Day 3B: Final Answer - {:#?}",
-        diagnostic.answer_b.expect("Unable to calculate answer - 3B."));
+    println!("Day 3A: Final Answer - {}", diagnostic.power_consumption);
 }
